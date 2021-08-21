36bfa43bdafcebbdbb6047c03b305d3c323d4da4
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-08-20 22:26:05 -0300

    Inserindo a AsciiArt no Console Application

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("``                        ```                            `   ``                                 ````");
+            Console.WriteLine("                       ```                              ````````                                 ```");
+            Console.WriteLine("                     `-/////sys+:``````````         ``````....`.`                                  `");
+            Console.WriteLine("                     .:soooyddmmmds/....````````````````...-----.                                   ");
+            Console.WriteLine("                     `../osmmmdmmhNNdo:........:........---------.                                  ");
+            Console.WriteLine("``                    .``:dmmNmN+/hmNNmy/-....-s:..-------::::----`                                 ");
+            Console.WriteLine("```                   `-.-hmmNNms+ymNNNNmho:..-so--------::::::----                                `");
+            Console.WriteLine("````                    .:dNNNNy//:syNmNmNNdo:-ss+------::::::/+:-:.                              ``");
+            Console.WriteLine("``````                 ..:+mNmdo.`-:.dmmmNmhhhysss:-----:::::/+++::/                                ");
+            Console.WriteLine("```````                ` .:mms+-....-hmmmds/hyNdss+----:::::/+/+++/o.                              `");
+            Console.WriteLine("````                     `-s/.`....-+:mmd++hNyhNmyo----::::/+//:/+oy+                             ` ");
+            Console.WriteLine("     ````                 `/syo:.....smy:-:yso/mNNs---::::/+++/:::+hy-                        ``   `");
+            Console.WriteLine("`````                     .sdmmh::-.-s//.-:y--+smNo--::::++++/::/::/y/.                        `````");
+            Console.WriteLine("```          `      ````  /dmmNNd:...+.--/+y//--/h+--::/o+++::/+hds/:++-               `````````````");
+            Console.WriteLine("-.`  ``````````  ````````-/mdNNmoo-..-.:+/+ss:-+ys:-:/ooo+/:://:smNNmhss/`           `````````````..");
+            Console.WriteLine(".......`````````````````-.yddmmsoo:..-/::+yy+oyys+:+ooo+/:://:--::odNNNmd/```````````````````.......");
+            Console.WriteLine("//:::::-------.``.`````..-syysdssss+oo+syhhhyhyys+oo+/:--:+::::/:--/ohNNmds:.```````````........----");
+            Console.WriteLine("+++////:::::---......--../:/--:+yyyhyyhhhyyssssso+/::-:/+o+++++oo+/:shddNNNdo:-.```````...-----:::::");
+            Console.WriteLine("sssso++////::::-.------:/:-:/--:-yhhhyo///+++++++//++syysoooooossyyyhh/-yNNNmys/-.....`..--:::://///");
+            Console.WriteLine("dddhhhyo+//////:-:::-://::/+////ohhhyysssooo+osssssyyhysssssssyyyyhhyo:--hNNNmmm+--...-:::::////++oo");
+            Console.WriteLine("ddddddhhyo+++///::---.-`.---:/:oyysoooo++++++/::::::/+o+/:::////oo++oyysosNNNNmmm///-::://///+osyhhh");
+            Console.WriteLine("sohddddddhs+++++++/:.:///++/+/+oy+:--::/++++++++/:::::/+o+/::::+oo+//o+/oyNNNNmmNh+d//////++syhhhddd");
+            Console.WriteLine(":ohdddddddhso/+o+:-/sss++hhhhddddmdo:----:/++oo+++/:::::/ooo++ooooooos/:/yNMNNmNNmyNdo++++/::---:+yh");
+            Console.WriteLine(":/+//+++++++:.-+s+oyysoshdddddddmmmmdo:---:osys+++////::::/oooooosooo++//mNNNmmNNNNNNmo+:.`````````-");
+            Console.WriteLine("yyhhdddhhhhyysosyssyyydmmdddmmmmmmmmmds---+yys::/oo+/++/::+ooooohmmho++odNNNNmNNNNNMNNh/---.....``  ");
+            Console.WriteLine("hhhhhddddhhhhhhhhyyhmmmmmmmmmmmmmmmmmmd/--+hy:--oyo+oso+oooooosdmmmNmddmmNNNNNNNNNNNNMmdso:--:-+.--/");
+            Console.WriteLine("hhhhhhhhdhhhhhhyyyydmhmmNNNNmmmmmNmmmmmh:-yy/-:sdy+++++syyssshmNNNNNNNmNNNNNNNNNNNddNNmmo:.--:/+osyy");
+            Console.WriteLine("hyyyyyyyyyyysooooo+hd+mNNNNNNNNNNNNmmNNNh+h/:ohdh+oooosyhhdmNMNNNNNNNNNNNNNNNNmdddhoNNmms-````:o-`-y");
+            Console.WriteLine("-.``.-/+oo++//:-.`ohd/-smNNNNNNNNNNmmNNNNhhydmmdosyssydNNMMMMNNNNNNNNNNNNNNmhdddh++smNhds++o+o+/-:+y");
+        }        
+    }
+}
